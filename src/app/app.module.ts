3edac062ae0c377ee4ce05c87eef2527506a3bf3
OlgaLavrishcheva <76204278+OlgaLavrishcheva@users.noreply.github.com>
2021-04-27 14:26:59 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CustomersHostComponent } from './customers-host/customers-host.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersHostComponent,
+    CustomersListComponent,
+    CustomerDetailsComponent
   ],
   imports: [
     BrowserModule
