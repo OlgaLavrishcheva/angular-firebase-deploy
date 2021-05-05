1452ffae0a52a8ad7be200fa0a1938abb1c67ca4
OlgaLavrishcheva <76204278+OlgaLavrishcheva@users.noreply.github.com>
2021-05-05 13:05:51 +0300

    применение abstract control для визуального упрощения кода

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CustomerService} from "../shared/customer.service";
+import {AbstractControl} from "@angular/forms";
 
 @Component({
   selector: 'app-customer-details',
@@ -8,10 +9,22 @@ import {CustomerService} from "../shared/customer.service";
 })
 export class CustomerDetailsComponent implements OnInit {
 
+  key: AbstractControl;
+  name: AbstractControl;
+  email: AbstractControl;
+  mobile: AbstractControl;
+  location: AbstractControl;
+
   constructor(public svc: CustomerService) {
   }
 
   ngOnInit(): void {
+    this.key = this.svc.form.controls.key;
+    this.name = this.svc.form.controls.name;
+    this.email = this.svc.form.controls.email;
+    this.mobile = this.svc.form.controls.mobile;
+    this.location = this.svc.form.controls.location;
+
     this.svc.form.controls.key.setValue(null);
     this.svc.form.controls.name.setValue('John Smith');
     this.svc.form.controls.email.setValue('john@gmail.com');
@@ -22,7 +35,7 @@ export class CustomerDetailsComponent implements OnInit {
   onSubmit(): void {
     if (this.svc.form.valid) {
       if (this.svc.form.controls.key.value === null) {
-      this.svc.createData();
+        this.svc.createData();
       }
     }
   }
